7244d8ae4ed11385a9b19a60a940a9cfe50c8d62
d-zer <dozer31415@gmail.com>
2022-08-15 00:10:15 +0300

    konsol programlama

---
+++ pratik_konsol_programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;			
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		Console.WriteLine("İsminizi Giriniz:");
+		string name = Console.ReadLine();
+		Console.WriteLine("Soyisminizi Giriniz:");
+		string surname = Console.ReadLine();
+		Console.WriteLine("Merhaba " + name + " " + surname);
+	}
+}
+
